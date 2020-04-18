2403570783e501205860ffac74eca41c23615877
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-04-18 18:30:41 -0400

    added parenthesis and commas for neater output

--- Calculators/Body Mass Index Calculator.py
+++ Calculators/Body Mass Index Calculator.py
@@ -7,9 +7,9 @@ Weight = float(input("Pounds: "))
 BMI = int(((703 * Weight) / (Inches**2)))
 
 if BMI < 19:
-    print("Your BMI is: " + str(BMI) + " You are underweight, you may need to gain weight.")
+    print("Your BMI is: (" + str(BMI) + "), You are underweight, you may need to gain weight.")
 else:
     if BMI > 25:
-        print("Your BMI is: " + str(BMI) + " You are overweight, you may need to lose weight.")
+        print("Your BMI is: (" + str(BMI) + "), You are overweight, you may need to lose weight.")
     else:
-        print("Your BMI is: " + str(BMI) + " You have a healthy weight.")
+        print("Your BMI is: (" + str(BMI) + "), You have a healthy weight.")
