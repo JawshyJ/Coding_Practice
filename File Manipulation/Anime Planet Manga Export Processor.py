a178a5ab82a9c900fbe6e60224c7b86f0a5e17fa
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-08-12 14:34:27 -0400

    Updated project to ensure alphabetical sorting and updated file output to look neater

--- File Manipulation/Anime Planet Manga Export Processor.py
+++ File Manipulation/Anime Planet Manga Export Processor.py
@@ -1,34 +1,61 @@
 # 8/4/2020 Anime Plane Manga Export Processor
 # The purpose of this program is to process all of the manga data from a JSON file into a neat and organized text file.
-# Tasks
-## Alphabetically organize objects
+
+versionDate = "8/12/2020"
 
 import json
 import datetime
+import operator
 import os
 from tkinter import filedialog
 
 name = filedialog.askopenfilename()
+currentDirectory = os.path.dirname(os.path.realpath(name))
 nameProcessed = os.path.splitext(os.path.basename(name))[0]
 count = 0
 time = datetime.datetime.now()
 
+class mangaObject:
+    def __init__(self, mname, status, started, completed, rating, volume, chapter):
+        self.name = mname
+        self.status = status
+        self.started = started
+        self.completed = completed
+        self.rating = rating
+        self.volume = volume
+        self.chapter = chapter
+
+mangaList = []
+
 with open(name) as json_file:
     data = json.load(json_file)
     user = data['user'].get("name")
-    fileName = (user + "_Anime-Planet_Manga-Data_" + str(time.month) + "-" + str(time.day) + "-" + str(time.year) + ").txt")
-    DataPort = open(fileName, "a+")
-    DataPort.write("====[Anime-Planet Manga Data]====")
+    fileName = (" Anime-Planet user_" + user + " Manga Data (" + str(time.month) + "-" + str(time.day) + "-" + str(time.year) + ").txt")
+    DataPort = open(os.path.join(currentDirectory, fileName), "w")
+    DataPort.write("=========[Anime-Planet Manga Data]=========")
     DataPort.write("\nName: " + user)
     DataPort.write("\nExport Date: " + data['export'].get("date"))
     DataPort.write("\nExport Version: " + data['export'].get("version"))
-    DataPort.write("\n=================================")
+    DataPort.write("\nCompatible Versions: (0.1a) as of " + versionDate)
+    DataPort.write("\n===========================================")
     for manga in data['entries']:
-        DataPort.write("\n---------------------------------")
+        tempName = str(manga['name'])
+        tempStatus = str(manga['status'])
+        tempStarted = str(manga['completed'])
+        tempCompleted = str(manga['completed'])
+        tempRating = str(manga['rating'])
+        tempVolume = str(manga['vol'])
+        tempChapter = str(manga['ch'])
+        tempManga = (mangaObject(tempName, tempStatus, tempStarted, tempCompleted, tempRating, tempVolume, tempChapter))
+        mangaList.append(tempManga)
+        count += 1
+    mangaList = sorted(mangaList, key=operator.attrgetter('name'))
+    for obj in mangaList:
+        DataPort.write("\n-------------------------------------------")
         try:
-            DataPort.write('\nName:      ' + str(manga['name']))
+            DataPort.write('\nName:      ' + obj.name)
         except UnicodeEncodeError:
-            problemString = str(manga['name'])
+            problemString = obj.name
             newName = ""
             for character in problemString:
                 if character.isalnum():
@@ -36,18 +63,17 @@ with open(name) as json_file:
                 else:
                     newName += "*"
             DataPort.write('\nName:      ' + str(newName))
-        DataPort.write('\nStatus:    ' + manga['status'])
-        DataPort.write('\nStarted:   ' + str(manga['started']))
-        DataPort.write('\nCompleted: ' + str(manga['completed']))
-        DataPort.write('\nRating:    ' + str(manga['rating']))
-        DataPort.write('\nVolume:    ' + str(manga['vol']))
-        DataPort.write('\nChapter:   ' + str(manga['ch']))
-        count += 1
+        DataPort.write('\nStatus:    ' + obj.status)
+        DataPort.write('\nStarted:   ' + obj.started)
+        DataPort.write('\nCompleted: ' + obj.completed)
+        DataPort.write('\nRating:    ' + obj.rating)
+        DataPort.write('\nVolume:     ' + obj.volume)
+        DataPort.write('\nChapter:  ' + obj.chapter)
 
-DataPort.write("\n=================================")
+DataPort.write("\n===========================================")
 DataPort.write("\nTotal Manga:  " + str(count))
 DataPort.write("\nCompleted at: " + str(time.month) + "/" + str(time.day) + "/" + str(time.year) + " (" + str(time.hour) + ":" + str(time.minute) + ")")
-DataPort.write("\n=================================")
+DataPort.write("\n===========================================")
 DataPort.close()
 count = 0
 print("Data written to new file: " + fileName)
