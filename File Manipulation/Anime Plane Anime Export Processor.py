b042cc86e1967a2072f5d36385dd26a18750e551
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-08-12 14:17:51 -0400

    Updated project to ensure alphabetical sorting

--- File Manipulation/Anime Plane Anime Export Processor.py
+++ File Manipulation/Anime Plane Anime Export Processor.py
@@ -1,34 +1,62 @@
 # 8/4/2020 Anime Plane Anime Export Processor
 # The purpose of this program is to process all of the anime data from a JSON file into a neat and organized text file.
-# Tasks
-## Alphabetically organize objects
+
+versionDate = "8/12/2020"
 
 import json
 import datetime
+import operator
 import os
 from tkinter import filedialog
 
 name = filedialog.askopenfilename()
+currentDirectory = os.path.dirname(os.path.realpath(name))
 nameProcessed = os.path.splitext(os.path.basename(name))[0]
 count = 0
 time = datetime.datetime.now()
+
+
+class animeObject:
+    def __init__(self, aname, status, started, completed, rating, times, eps):
+        self.name = aname
+        self.status = status
+        self.started = started
+        self.completed = completed
+        self.rating = rating
+        self.times = times
+        self.eps = eps
+
+animeList = []
 
 with open(name) as json_file:
     data = json.load(json_file)
     user = data['user'].get("name")
     fileName = (user + "_Anime-Planet_Anime-Data_" + str(time.month) + "-" + str(time.day) + "-" + str(time.year) + ").txt")
-    DataPort = open(fileName, "a+")
-    DataPort.write("====[Anime-Planet Anime Data]====")
+    DataPort = open(os.path.join(currentDirectory, fileName), "w")
+    DataPort.write("=========[Anime-Planet Anime Data]=========")
     DataPort.write("\nName: " + user)
     DataPort.write("\nExport Date: " + data['export'].get("date"))
     DataPort.write("\nExport Version: " + data['export'].get("version"))
-    DataPort.write("\n=================================")
+    DataPort.write("\nCompatible Versions: (0.1a) as of " + versionDate)
+    DataPort.write("\n===========================================")
     for anime in data['entries']:
-        DataPort.write("\n---------------------------------")
+        tempName = str(anime['name'])
+        tempStatus = str(anime['status'])
+        tempStarted = str(anime['started'])
+        tempCompleted = str(anime['completed'])
+        tempRating = str(anime['rating'])
+        tempTimes = str(anime['times'])
+        tempEps = str(anime['eps'])
+        tempAnime = (animeObject(tempName, tempStatus, tempStarted, tempCompleted, tempRating, tempTimes, tempEps))
+        animeList.append(tempAnime)
+        count += 1
+    animeList = sorted(animeList, key=operator.attrgetter('name')) # Sorts the anime alphabetically
+    for obj in animeList:
+        DataPort.write("\n-------------------------------------------")
         try:
-            DataPort.write('\nName:      ' + str(anime['name']))
+            DataPort.write('\nName:      ' + obj.name)
         except UnicodeEncodeError:
-            problemString = str(anime['name'])
+            problemString = obj.name
             newName = ""
             for character in problemString:
                 if character.isalnum():
@@ -36,18 +64,16 @@ with open(name) as json_file:
                 else:
                     newName += "*"
             DataPort.write('\nName:      ' + str(newName))
-        DataPort.write('\nStatus:    ' + str(anime['status']))
-        DataPort.write('\nStarted:   ' + str(anime['started']))
-        DataPort.write('\nCompleted: ' + str(anime['completed']))
-        DataPort.write('\nRating:    ' + str(anime['rating']))
-        DataPort.write('\nTimes:     ' + str(anime['times']))
-        DataPort.write('\nEpisodes:  ' + str(anime['eps']))
-        count += 1
+        DataPort.write('\nStatus:    ' + obj.status)
+        DataPort.write('\nStarted:   ' + obj.started)
+        DataPort.write('\nCompleted: ' + obj.completed)
+        DataPort.write('\nRating:    ' + obj.rating)
+        DataPort.write('\nTimes:     ' + obj.times)
+        DataPort.write('\nEpisodes:  ' + obj.eps)
 
-DataPort.write("\n=================================")
+DataPort.write("\n===========================================")
 DataPort.write("\nTotal Anime:  " + str(count))
 DataPort.write("\nCompleted at: " + str(time.month) + "/" + str(time.day) + "/" + str(time.year) + " (" + str(time.hour) + ":" + str(time.minute) + ")")
-DataPort.write("\n=================================")
+DataPort.write("\n===========================================")
 DataPort.close()
 count = 0
-print("Data written to new file: " + fileName)
