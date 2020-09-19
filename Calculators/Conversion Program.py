c1c32a94e1a420d943c94fb65a58f847dfd6405e
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-09-19 10:39:07 -0400

    Following through with a conversion program I decided to make a while back

---
+++ Calculators/Conversion Program.py
@@ -0,0 +1,5 @@
+#4/27/2020 - Conversion Program
+
+# The purpose of this program is to let a user convert anything they'd like.
+# It'll be supported via the use of a user interface.
+# The user interface will include a browsing area and the conversion area.
