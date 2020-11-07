27893779efd1319820b6353bc23bb85d611665e0
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-11-07 07:38:21 -0500

    Preparing code structure

--- Games/Overlord.py
+++ Games/Overlord.py
@@ -1,3 +1,19 @@
 #11/3/2020 - Overlord
-# A game where a user takes control of a small military group during World War III
+# A game where a user takes control of a small military cell during World War III
 
+game = True
+bankYN = ["yes", "no"]
+
+userInput = ""
+
+def InputCheck(phrase, listName):
+    while True:
+        text = input(phrase)
+        if text.lower() in listName:
+            return text
+
+
+while game is True:
+    print("Welcome to Overlord.")
+    userInput = InputCheck("Type yes or no\n", bankYN)
+    game = False
