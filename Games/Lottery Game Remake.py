2c90eb9626785f50b692ab5ced21bf855480dd2d
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-11-07 06:55:18 -0500

    Remade one of my first programming projects

---
+++ Games/Lottery Game Remake.py
@@ -0,0 +1,31 @@
+# 11/7/2020 - Lottery Game Remake
+# Program generates a random number between 1 and 20. The user has to guess which number it is.
+
+import random
+
+def CheckInput(guess, actual):
+    if guess == actual:
+        return True
+    else:
+        return False
+
+MysteryNumber = random.randint(1, 20)
+userInput = 0
+guesses = 0
+Won = False
+
+while guesses < 3:
+    userInput = int(input("Guess a number 1 - 20. (" + str(3 - guesses) + " attempt(s) left)\n"))
+    if CheckInput(userInput, MysteryNumber) is True:
+        print("You guessed the correct number. You've won!")
+        Won = True
+        break
+    else:
+        guesses += 1
+        if guesses == 0:
+            break
+        else:
+            print("You've guessed the wrong number. Try again.")
+
+if Won is False:
+    print("You lost, the number was: " + str(MysteryNumber))
