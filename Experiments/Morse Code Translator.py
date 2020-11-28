8e8f5bd0c3c71e353154151825d31a00e15e57f8
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-11-28 16:17:17 -0500

    Began morse code software project

---
+++ Experiments/Morse Code Translator.py
@@ -0,0 +1,6 @@
+# 11/28/2020 - Morse Code Translator
+# The goal of this project is to create a clean, working Morse code translation software.
+# To-Do
+## Create a text input and a text display box, side by side with a gap
+## Add a 'play' button somewhere to play out the morse code audio
+## Add a 'switch button in order to switch from translating from text to morse or morse to text (user can paste morse)
