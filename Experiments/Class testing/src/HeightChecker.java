8e62e34e79be7885782e64007a160cf5983a526c
JawshyJ <Josh_P_Johnson@Outlook.com>
2020-12-06 07:42:27 -0500

    Started a new project to refresh my java knowledge

---
+++ Experiments/Class testing/src/HeightChecker.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class HeightChecker{
+    public static void main(String[] args){
+        String name = "";
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = input.next();
+        System.out.println("Sup, " + name + "\nThis program is incomplete, but I'll have it done. Eventually.");
+    }
+}
