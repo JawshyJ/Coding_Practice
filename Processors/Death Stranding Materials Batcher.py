eb6b229d41aa49b1545af2120e6bee8e982adb41
Josh <Josh_P_Johnson@Outlook.com>
2023-02-06 16:22:58 -0500

    Added cleaner output to help with future process return.

--- Processors/Death Stranding Materials Batcher.py
+++ Processors/Death Stranding Materials Batcher.py
@@ -18,61 +18,64 @@ amount = 0
 
 
 def calculate_batches(mat_list, amount):
-  initial_amount = amount
-  current_total = amount
-  batches = []
-  sum = 0
-  for batch in mat_list:
-    if amount >= batch:
-      batches.append((batch, trunc(amount / batch)))
-      amount = amount - (batch * trunc(amount / batch))
-  if amount > 0 and amount < mat_list[len(mat_list) - 1]:
-    if batches[len(batches) - 1][0] == mat_list[len(mat_list) - 1]:
-      batches[len(batches) - 1] = (mat_list[len(mat_list) - 1],
-                                   batches[len(batches) - 1][1] + 1)
+    initial_amount = amount
+    current_total = amount
+    batches = []
+    clean_list = []
+    recommended_sum = 0
+    for batch in mat_list:
+        if amount >= batch:
+            batches.append((batch, trunc(amount / batch)))
+            amount = amount - (batch * trunc(amount / batch))
+    if 0 < amount < mat_list[len(mat_list) - 1]:
+        if batches[len(batches) - 1][0] == mat_list[len(mat_list) - 1]:
+            batches[len(batches) - 1] = (mat_list[len(mat_list) - 1],
+                                         batches[len(batches) - 1][1] + 1)
+        else:
+            batches.append((mat_list[len(mat_list) - 1], 1))
+    # If there are two of the smallest batch numbers, remove them and add to the previous batch size amount
+    if batches[len(batches) - 1][0] == mat_list[len(mat_list) -
+                                                1] and batches[len(batches) -
+                                                               1][1] == 2:
+        batches.pop()
+        print("smallest batch popped.")
+        if batches[len(batches) - 1][0] == mat_list[len(mat_list) - 2]:
+            print("Adding to the 2nd smallest batch.")
+            batches[len(batches) - 1] = (mat_list[len(mat_list) - 2],
+                                         batches[len(batches) - 1][1] + 1)
+        else:
+            print("Adding 2nd smallest batch to recommended batches.")
+            batches.append((mat_list[len(mat_list) - 2], 1))
+    for item in batches:
+        recommended_sum = recommended_sum + (item[0] * item[1])
+        clean_list.append((str(item[0]) + " x " + str(item[1])))
+        print(
+            str(item[0]) + ": " + str(item[1]) + " (" + f"{current_total:,}" +
+            " - " + f"{item[0] * item[1]:,}" + " = " +
+            f"{current_total - (item[0] * item[1]):,}" + ")")
+        current_total = current_total - (item[0] * item[1])
+    if recommended_sum == initial_amount:
+        print("Perfect batch, no surplus\n")
     else:
-      batches.append((mat_list[len(mat_list) - 1], 1))
-  # If there are two of the smallest batch numbers, remove them and add to the previous batch size amount
-  if batches[len(batches) - 1][0] == mat_list[len(mat_list) -
-                                              1] and batches[len(batches) -
-                                                             1][1] == 2:
-    batches.pop()
-    print("smallest batch popped.")
-    if batches[len(batches) - 1][0] == mat_list[len(mat_list) - 2]:
-      print("Adding to the 2nd smallest batch.")
-      batches[len(batches) - 1] = (mat_list[len(mat_list) - 2],
-                                   batches[len(batches) - 1][1] + 1)
-    else:
-      print("Adding 2nd smallest batch to recommended batches.")
-      batches.append((mat_list[len(mat_list) - 2], 1))
-  for item in batches:
-    sum = sum + (item[0] * item[1])
-    print(
-      str(item[0]) + ": " + str(item[1]) + " (" + f"{current_total:,}" +
-      " - " + f"{item[0] * item[1]:,}" + " = " +
-      f"{current_total - (item[0] * item[1]):,}" + ")")
-    current_total = current_total - (item[0] * item[1])
-  if sum == initial_amount:
-    print("Perfect batch, no surplus\n")
-  else:
-    print(f"Surplus: {sum:,} - {initial_amount:,} = {sum - initial_amount:,}")
+        print(f"Surplus: {recommended_sum:,} - {initial_amount:,} = {recommended_sum - initial_amount:,}")
+    print("[Paste List Below:]\n" + ', '.join(clean_list))
 
 
 # User Menu
 while True:
-  try:
-    user_input = input("Select a Material:\n" + material_list +
-                       "\n(Quit)\n\n").lower()
-  except Exception:
-    print("Invalid input. Select One:\n" + material_list + "\n(Quit)\n\n")
-  if user_input == "metal":
-    material = metals
-  elif user_input == "ceramic":
-    material = ceramics
-  elif user_input == "quit":
-    print("Ending program.")
-    break
-  else:
-    print("Select a Material:\n" + material_list + "\n\n")
-  amount = int(input("\nEnter the amount needed:\n"))
-  calculate_batches(material, amount)
+    try:
+        user_input = input("Select a Material:\n" + material_list +
+                           "\n(Quit)\n\n").lower()
+    except Exception:
+        print("Invalid input. Select One:\n" + material_list + "\n(Quit)\n\n")
+    if user_input == "metal":
+        material = metals
+    elif user_input == "ceramic":
+        material = ceramics
+    elif user_input == "quit":
+        print("Ending program.")
+        break
+    else:
+        print("Select a Material:\n" + material_list + "\n\n")
+    amount = int(input("\nEnter the amount needed:\n"))
+    calculate_batches(material, amount)
